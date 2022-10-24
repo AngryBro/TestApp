ab8d85897a7af32e728042f85845de2a32d71f90
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-10-24 21:57:37 +1000

    Update StudentDataController.php

--- backend/app/Http/Controllers/StudentDataController.php
+++ backend/app/Http/Controllers/StudentDataController.php
@@ -4,10 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Validator;
 
 class StudentDataController extends Controller
 {
-    function getData(Request $request) {
-        
+    function loginUpdate(Request $request) {
+        $token = $request->bearerToken();
+        $validator = Validator::make($request->all(),[
+            'login' => 'required|min:3',
+        ]);
+        if($validator->fails()) return response()->json([
+            'message' => 'invalid login'
+        ],422);
+        $login = $validator->validated()['login'];
+        if(User::firstWhere('login',$login)!==null) {
+            return response()->json([
+                'message' => "login $login already taken"
+            ],400);
+        }
+        $user = User::firstWhere('remember_token',$token);
+        $user->login = $login;
+        $user->save();
+        return response()->json([]);
     }
 }
